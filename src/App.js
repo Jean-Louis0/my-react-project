3503685a530676d0257d6196e2b1615d0826c9ba
Jean-Louis0 <118429791+Jean-Louis0@users.noreply.github.com>
2023-02-17 09:33:45 +0100

    3eme modif

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Accueil from './pages/Accueil';
-
 function App() {
   return (
-    <BrowserRouter>
-      <routes>
-        <Route path='/' element={<Accueil/>} />
-      </routes>
-    </BrowserRouter>
-
-  );
+    <div>
+      
+    </div>
+    );
 }
 
 export default App;
